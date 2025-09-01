cd543df1d6434d2dbc50678a6020abc445009f9a
yuzhayo <yuzha.yo@gmail.com>
2025-09-02 01:05:56 +0700

    Delete src directory
    x

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-﻿/* IMPORT SECTION */
-import LauncherScreen from "./Core/Launcher/LauncherScreen";
-
-/* UI SECTION */
-export default function App() {
-  return <LauncherScreen />; /* wrapper only, main screen */
-}
--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-const root = document.getElementById("root")!;
-createRoot(root).render(<App />);
